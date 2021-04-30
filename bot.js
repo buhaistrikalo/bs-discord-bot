0f303547c37ad3e7357e05051e718c2f835aceb3
BuhaiStrikalo <kirilltcoi@yandex.ru>
2021-04-30 21:10:26 +0300

    added command !userinfo (embed)

--- bot.js
+++ bot.js
@@ -10,15 +10,32 @@ const bot = new Discord.Client({disableEveryone: true});
   
   bot.on('message', msg => {
     if(msg.author.bot) return; //проверка, чтобы бот не реагировал на сообщения других ботов
+    if(!msg.content.startsWith(prefix)) return; //пропускать сообщения без префикса
+
+
     let messageArray = msg.content.split(" ");
     let cmd = messageArray[0];
     let args = messageArray.slice(1);
 
 
-    if (msg.content === prefix + 'сухой') {
+    if (cmd === prefix + 'дневальный') {
       msg.reply('ЯЯЯЯЯЯЯЯЯЯЯЯЯЯЯ!');
     }
-  
+    if (cmd === prefix + 'userinfo') {
+      let embed = new Discord.MessageEmbed() 
+        //.setImage(msg.author.avatarURL)
+        .setColor("#985986")
+        .setAuthor(msg.author.username)
+        .setDescription("Информация об игроке")
+        .addFields(
+          {name: 'PvE Raid progress', value: '10/10', inline: true}, 
+          {name: 'PvE Dungeons progress', value: 'key 13 - 13:32', inline: true},
+          {name: 'PvP Place: 3', value: 'Raiting: 2353', inline: true}
+        )
+        .setFooter('Если вы заметили какое-либо нарушение правил данным игроком, напишите мне @Сухой')
+      msg.channel.send(embed)
+    }
+    console.log(msg.author.avatarURL)
     /*if (cmd === prefix + 'командуй') {
      msg.reply('Ротa, ' + args + ' !');
     }*/
