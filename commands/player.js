bd00ba9c22a88abb199ff38da97b922aa478b680
BuhaiStrikalo <kirilltcoi@yandex.ru>
2021-05-06 17:25:15 +0300

    !player >> added clan

--- commands/player.js
+++ commands/player.js
@@ -4,31 +4,34 @@ module.exports = {
     name: 'player',
     description: 'Search player',
     async execute(msg, args) {
-        let players = require('../players/players.json');
+        let players = require('../database/players.json');
         let player = msg.mentions.users.first();
         let playerid = player.id;
+        let PL = players[playerid]
+        
         try {
-            if (players[playerid]) {
-                let PL = players[playerid]
+            if (PL) {
                 let embed = new Discord.MessageEmbed() 
                 .setThumbnail(player.displayAvatarURL())
                 .setColor("#4169E1")
                 .setAuthor(PL.name)
-                .setDescription("Level: **" + PL.level + "**")
+                .setDescription(`Level: **${PL.level}**`)
                 .addFields(
                 {name: 'PvE Bosses', value: PL.pve_raid, inline: true}, 
                 {name: 'PvE Dung', value: PL.pve_dung, inline: true},
-                {name: 'PvP Raiting', value: '**' + PL.pvp + '**', inline: true}
+                {name: 'PvP Raiting', value: `**${PL.pvp}**`, inline: true}
                 )
-                .setFooter(PL.clan, 'https://i.pinimg.com/originals/12/e4/d8/12e4d8059bcc09c0391ccf5854b68b37.png');
+                if (PL.clan){   
+                    let clans = require('../database/clans.json'); 
+                    embed.setFooter(clans[PL.clan].name, clans[PL.clan].icon)
+                } 
                 msg.channel.send(embed)
-                } else {
-                    msg.channel.send(args + ' player not founded')
-                }
-
-            } 
-            catch (err) {
-                throw err;
+            } else {
+                msg.channel.send(args + ' player not founded')
             }
+
+        } catch (err) {
+            throw err;
+        }
     }
 }
