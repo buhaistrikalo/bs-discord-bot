5b1e9b19e461a84388e4be1bd00662386adc4e26
BuhaiStrikalo <kirilltcoi@yandex.ru>
2021-05-06 16:14:37 +0300

    !player - avatar addded

--- commands/player.js
+++ commands/player.js
@@ -5,21 +5,22 @@ module.exports = {
     description: 'Search player',
     async execute(msg, args) {
         let players = require('../players/players.json');
-        let playerid = msg.content.split('!')[2].slice(0,-1);
+        let player = msg.mentions.users.first();
+        let playerid = player.id;
         try {
             if (players[playerid]) {
                 let PL = players[playerid]
                 let embed = new Discord.MessageEmbed() 
-                //.setImage(msg.playerid.avatarURL)
-                .setColor("#ff0000")
+                .setThumbnail(player.displayAvatarURL())
+                .setColor("#4169E1")
                 .setAuthor(PL.name)
                 .setDescription("Level: **" + PL.level + "**")
                 .addFields(
-                {name: 'PvE Raid progress', value: PL.pve_raid, inline: true}, 
-                {name: 'PvE Dungeons progress', value: PL.pve_dung, inline: true},
+                {name: 'PvE Bosses', value: PL.pve_raid, inline: true}, 
+                {name: 'PvE Dung', value: PL.pve_dung, inline: true},
                 {name: 'PvP Raiting', value: '**' + PL.pvp + '**', inline: true}
                 )
-                .setFooter('Если вы заметили какое-либо нарушение правил данным игроком, напишите мне @Сухой')
+                .setFooter(PL.clan, 'https://i.pinimg.com/originals/12/e4/d8/12e4d8059bcc09c0391ccf5854b68b37.png');
                 msg.channel.send(embed)
                 } else {
                     msg.channel.send(args + ' player not founded')
