0b8b0939624cc15d809e464c0f554a7197b36808
Kirill <kirilltcoi@yandex.ru>
2021-07-08 00:59:31 +0800

    /rank

--- gladosbot.js
+++ gladosbot.js
@@ -15,41 +15,90 @@ client.on('ready', async () => {
 
     const commands = await getApp().commands.get()
 
-    for(const cmd of commands){ //delete all commands
-        await getApp().commands(cmd.id).delete()
-    }
+    //delete all commands
+    // for(const cmd of commands){ 
+    //     await getApp().commands(cmd.id).delete()
+    // }
 
-    // await getApp().commands.post({
-    //     data: {
-    //         name: 'ping',
-    //         description: 'command',
-    //     },
-    // })
+    await getApp().commands.post({
+        data: {
+            name: 'ping',
+            description: 'command',
+        },
+    });
+
+    await getApp().commands.post({
+        data: {
+            name: 'topplayers',
+            description: 'Highest levels'
+        },
+    });
+
+    await getApp().commands.post({
+        data: {
+            name: 'rank',
+            description: 'write your account ID and find out where you are in the ranking by level',
+            options: [
+                {
+                    name: 'account',
+                    description: 'Account ID',
+                    require: false,
+                    type: 4
+                }
+            ]
+        }
+    });
 
     client.ws.on('INTERACTION_CREATE', async (interaction) => {
-        const command = interaction.data.name.toLowerCase()
+        const { name, options } = interaction.data
+
+        const command = name.toLowerCase()
+
+        const args = {}
+
+        if (options) {
+            for (const option of options) {
+                const { name, value } = option
+                args[name] = value
+            }
+        }
+
+        if (command === 'rank') {
+            //lf account
+            // args = { account: 1 }
+        }
         if (command === 'ping') {
-            client.api.interactions(interaction.id, interaction.token).callback.post({
-                data: {
-                    type: 4,
-                    data: {
-                        content: "pong"
-                    }
-                }
-            })
+            reply(interaction,'pong')
         }
     })
 
-    // const reply = (interaction, response) => {
-    //     client.api.interaction(interaction.id, interaction.token).callback.post({
-    //         data: {
-    //             type: 4,
-    //             data: {
-    //                 content: response,
-    //             },
-    //         },
-    //     })
-    // }
+    const reply = (interaction, response) => {
+        let data = {
+            content: response,
+        }
+
+        //For embeds
+        if (typeof response === 'object') {
+            data = await createAPIMessage(interaction,response)
+        }
+
+        client.api.interactions(interaction.id, interaction.token).callback.post({
+            data: {
+                type: 4,
+                data,
+            }
+        })
+    }
 })
+
+const createAPIMessage = async (interaction, content) => {
+    const {} = await DiscordJS.APIMessage.create(
+        client.channels.resolve(interaction.channel_id).
+        content
+    )
+    .resolveDate()
+    .resolveFiles()
+    return { ...data, files }
+}
 
 client.login(process.env.TOKEN)
